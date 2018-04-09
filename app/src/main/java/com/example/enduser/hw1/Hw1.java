9b2a60fc42fd626f396f99c172beb5d9ec985295
zphupro <38090459+zphupro@users.noreply.github.com>
2018-04-09 00:35:54 -0700

    Initial commit

---
+++ app/src/main/java/com/example/enduser/hw1/Hw1.java
@@ -0,0 +1,13 @@
+package com.example.enduser.hw1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Hw1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hw1);
+    }
+}
